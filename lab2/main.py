a4e93d66ae37986e04a083c521cddec61100edf8
atemmel <temmeladam@gmail.com>
2021-02-05 16:56:34 +0100

    Did more work :)

--- lab2/main.py
+++ lab2/main.py
@@ -1,16 +1,21 @@
 #!/bin/python
 
-# nya frankie
-def frankie(target, names):
-    return np.array(list(map(lambda x: np.where(names == x)[0][0], target)))
-
+from six import StringIO
 from scipy.io import arff
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_text
+from sklearn.tree import export_graphviz
+from IPython.display import Image
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
+import pydotplus
 import pandas as pd
 import numpy as np
+
+# nya frankie
+def frankie(target, names):
+    return np.array(list(map(lambda x: np.where(names == x)[0][0], target)))
 
 def extract_column(df, index):
     return df[df.columns[index]]
@@ -24,66 +29,76 @@ def build_labelencoders(df):
         arr.append(le)
     return arr
 
+def build_onehotencoders(df):
+    ohe1 = OneHotEncoder()
+    ohe1.fit(df.iloc[:, :-1])
+    ohe2 = OneHotEncoder()
+    ohe2.fit(df.iloc[:, -1:])
+    #print(ohe.transform(df)[:5, :])
+    return ohe1, ohe2
+
+def create_tree_using_labelencoders(df):
+    les = build_labelencoders(df)
+    res = []
+    for i in range(0, len(df.columns)):
+        col = df.iloc[:, i].values
+        res.append(les[i].transform(col))
+    res = pd.DataFrame(res)
+
+    #print(res.transpose())
+    #inverse_res = []
+    #for i in range(0, len(df.columns)):
+        #col = res.iloc[i, :].values
+        #inverse_res.append(les[i].inverse_transform(col))
+    #inverse_res = pd.DataFrame(inverse_res)
+    #print(inverse_res.transpose())
+    res = res.transpose()
+    data = res.iloc[:,:-1]
+    target = res.iloc[:,-1:]
+    feature_names = list(df.columns[:-1])
+    class_names = list(extract_column(df, -1).unique())
+    return data, target, feature_names, class_names
+
+def create_tree_using_onehotencoders(df):
+    oheData, oheTarget = build_onehotencoders(df)
+    res = pd.DataFrame(oheData.transform(df.iloc[:, :-1]).toarray())
+    data = res.iloc[:, :-1]
+    res = pd.DataFrame(oheTarget.transform(df.iloc[:, -1:]).toarray())
+    target = res.iloc[:,-1:]
+    feature_names = oheData.get_feature_names(list(df.columns[:-1]))[:-1]
+    #print(feature_names)
+    #class_names = list(map(lambda x: x.decode("utf8"), list(extract_column(df, -1).unique())))
+    class_names = list(extract_column(df, -1).unique())
+    return data, target, feature_names, class_names
+
+
 filedata = arff.loadarff('./breast-cancer.arff')
 
 df = pd.DataFrame(filedata[0])
-
-les = build_labelencoders(df)
-res = []
 for i in range(0, len(df.columns)):
-    col = df.iloc[:, i].values
-    res.append(les[i].transform(col))
-res = pd.DataFrame(res)
+    title = list(df.columns)[i]
+    df[title] = df[title].apply(lambda s: s.decode("utf-8"))
 
-#print(res.transpose())
+#data, target, feature_names, class_names = create_tree_using_labelencoders(df)
+data, target, feature_names, class_names = create_tree_using_onehotencoders(df)
 
-inverse_res = []
-for i in range(0, len(df.columns)):
-    col = res.iloc[i, :].values
-    #print(col)
-    inverse_res.append(les[i].inverse_transform(col))
-inverse_res = pd.DataFrame(inverse_res)
-
-#print(inverse_res.transpose())
-
-res = res.transpose()
-data = res.iloc[:,:-1]
-target = res.iloc[:,-1:]
-
-#print(data)
-#print(target)
-
-#le = LabelEncoder()
-
-#data = df.iloc[:,:-1]
-#feature_names = df.Class.unique()
-#le.fit(feature_names)
-#target = df.iloc[:,-1:].Class
-#target = le.transform(target.values)
-
-#print(list(le.classes_))
-#print()
-#transformed = le.transform(df.iloc[:,-1].values[0:3])
-#print(transformed)
-
-#print(feature_names)
-#print(target)
-
-#index = 1
-#test = extract_column(df, 1)
-#print(test)
-#print()
-#print(list(le.inverse_transform(target)))
-
-decision_tree = DecisionTreeClassifier(random_state=0, max_depth=2)
+decision_tree = DecisionTreeClassifier(random_state=0, max_depth=4)
+#decision_tree = DecisionTreeClassifier()
 decision_tree = decision_tree.fit(data, target)
 
-#feature_names = df[df.columns[-1]].unique()
-#feature_names = extract_column(df, -1).unique()
-feature_names = list(df.columns[:-1])
-class_names = list(df.columns[-1:])
-#print(feature_names)
+#r = export_text(decision_tree, feature_names)
+#print(r)
 
-#r = export_text(decision_tree, feature_names=feature_names)
-r = export_text(decision_tree)
-print(r)
+dot_data = StringIO()
+
+export_graphviz(decision_tree, 
+        out_file=dot_data,
+        filled=True,
+        rounded=True,
+        special_characters=True,
+        feature_names=feature_names,
+        class_names=class_names)
+
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_png("big_tree.png")
+Image(graph.create_png())
